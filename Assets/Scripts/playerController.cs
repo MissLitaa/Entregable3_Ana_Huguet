61b43b7f787a00870daed5531dbe2fdf3dfba8ef
MissLitaa <anahug49@gmail.com>
2021-10-25 11:32:13 +0200

    Entregable3
    Entregable3

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -18,19 +18,21 @@ public class playerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+       //Escalado +0.5
+
         if (Input.GetKeyDown(KeyCode.X))
         {
-            transform.localScale.x + new Vector3(scaleX, transform.localScale.y, transform.localScale.z);
+            transform.localScale += new Vector3(scaleX, transform.localScale.y, transform.localScale.z);
         }
 
         if (Input.GetKeyDown(KeyCode.Y))
         {
-            transform.localScale.y + new Vector3(transform.localScale.x, scaleY, transform.localScale.z);
+            transform.localScale += new Vector3(transform.localScale.x, scaleY, transform.localScale.z);
         }
 
         if (Input.GetKeyDown(KeyCode.Z))
         {
-            transform.scale.z + new Vector3(transform.localScale.x, transform.localScale.y, scaleZ);
+            transform.localScale += new Vector3(transform.localScale.x, transform.localScale.y, scaleZ);
         }
 
     }
